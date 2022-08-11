dd18ca3bec0a3583ff1f713dd599a16d686bd3a3
zephyrus21 <pandey.piyus@gmail.com>
2022-08-11 11:53:04 +0530

    added apis

--- src/pages/api/test.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { Aadhar } from "src/db/uid";
-import { UID } from "src/types/data";
-
-const handler = (req: NextApiRequest, res: NextApiResponse<UID>) => {
-  res.status(200).json(Aadhar);
-};
-
-export default handler;
