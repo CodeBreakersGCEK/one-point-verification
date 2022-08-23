38cdefb202573996e339f2ed0a77cf81d7a50336
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-08-23 15:38:08 +0530

    student

--- src/pages/api/student/signup.ts
+++ src/pages/api/student/signup.ts
@@ -3,17 +3,25 @@ import connectMongo from '@utils/connectMongo';
 import bcrypt from 'bcryptjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import StudentSchema from '../../../models/student';
-
+import AadharSchema from 'src/models/aadhar';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   await connectMongo();
   const { method } = req;
-
+  const { uid } = req.body;
   switch (method) {
     case 'POST':
       try {
+        const isValid = await AadharSchema.findOne({ uid: uid });
+        if (!isValid) {
+          res.status(200).json({
+            message: 'Invalid Aadhar',
+            status: 'error',
+          });
+          return;
+        }
         const student = await StudentSchema.findOne({ uid: req.body.uid });
         if (student) {
           res.status(200).json({ message: 'Student  Exist', status: 'error' });
