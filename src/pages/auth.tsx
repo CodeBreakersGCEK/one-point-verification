d73a064ff1bdc9269e89e0d0bfa425c4264601e9
zephyrus21 <pandey.piyus@gmail.com>
2022-08-23 15:57:32 +0530

    removed scroll y

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -8,7 +8,7 @@ const Auth = () => {
 
   return (
     <Layout title="SignIn" type="auth">
-      <div className="flex items-center justify-center min-h-[80vh] overflow-y-scroll">
+      <div className="flex items-center justify-center min-h-[80vh]">
         {isUser ? (
           <UserForm setIsUser={setIsUser} setData={setData} data={data} /> // AICTE users
         ) : (
