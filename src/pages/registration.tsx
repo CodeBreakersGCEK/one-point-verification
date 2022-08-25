8a0ab715da882720d50a66044c94d1954962ff6f
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-08-25 19:02:48 +0530

    ioi

---
+++ src/pages/registration.tsx
@@ -0,0 +1,13 @@
+import Layout from '@components/Layout';
+import RegistrationForm from '@components/RegistrationForm';
+import React from 'react';
+
+const Registration = () => {
+  return (
+    <Layout title="One Verify | Registration">
+      <RegistrationForm />
+    </Layout>
+  );
+};
+
+export default Registration;
